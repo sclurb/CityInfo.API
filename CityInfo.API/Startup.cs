e8911051de23c913761645caab62a74c577b7109
sclurb <sclurb@outlook.com>
2020-09-09 13:55:13 -0400

    Completed modules 3 and 4

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -15,6 +17,20 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc()
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                   
+                });
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolover = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        castedResolover.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +44,9 @@ namespace CityInfo.API
             {
                 app.UseExceptionHandler();
             }
+            app.UseStatusCodePages();
+            app.UseMvc();
 
-            app.Run( (context) =>
-            {
-                //await context.Response.WriteAsync("Hello World!");
-                throw new Exception("Example Exception");
-            });
         }
     }
 }
